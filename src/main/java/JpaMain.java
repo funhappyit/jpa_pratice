7c4c0d17492472969262e6755d04c44ac5eb1fa3
“funhappyit” <fun.happyit@gmail.com>
2024-03-21 23:24:04 +0900

    ds

---
+++ src/main/java/JpaMain.java
@@ -0,0 +1,32 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // [엔티티 메니저 팩토리]- 생성 JPA
+        // 엔티티 메니저 팩토리는 애플리케이션 전체에서 딱 한 번만 생성하고 공유해서 사용해야 한다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        //[엔티티 메니저] - 생성
+        //엔티티 메니저를 사용해서 엔티티를 데이터베이스에 등록/수정/삭제/조회할 수 있다.
+        EntityManager em = emf.createEntityManager();
+        //[트랜잭션] - 획득
+        EntityTransaction tx = em.getTransaction();
+        try{
+            tx.begin(); //[트랜잭션] -시작
+            logic(em); //비즈니스 로직 실행
+            tx.commit(); //[트랜잭션] - 커밋
+        }catch (Exception e){
+            tx.rollback(); //[트랜잭션] - 롤백
+        }finally {
+            em.close(); //[엔티티 메니저] - 종료
+        }
+        emf.close(); //[엔티티 매니저 팩토리] - 종료
+    }
+
+    //비즈닌스 로직
+    private static void logic(EntityManager em){
+
+    }
+}
