0d402ed898138f11718266841aa7e6a1cf92950e
“funhappyit” <fun.happyit@gmail.com>
2024-03-24 20:58:36 +0900

    20240324 일대다 컬렉션 조회

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         try{
             tx.begin(); //[트랜잭션] -시작
-            deleteRelation(em); //비즈니스 로직 실행
+            biDirection(em); //비즈니스 로직 실행
             tx.commit(); //[트랜잭션] - 커밋
         }catch (Exception e){
             tx.rollback(); //[트랜잭션] - 롤백
@@ -77,6 +77,16 @@ public class JpaMain {
         Team team = em.find(Team.class,"team1");
         em.remove(team);
 
+    }
+
+    //일대다 방향으로 객체 그래프 탐색
+    public static void biDirection(EntityManager em){
+        Team team = em.find(Team.class,"team1");
+        List<Member> members = team.getMembers(); //(팀->회원) 객체 그래프 탐색
+
+        for(Member member:members){
+            System.out.println("member.username"+member.getUsername());
+        }
     }
 
 }
--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -3,17 +3,33 @@ package model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
     @Id
     @Column(name="TEAM_ID")
     private String id;
+
     private String name;
+
+    //추가
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
 
     public Team(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 
     public Team() {
