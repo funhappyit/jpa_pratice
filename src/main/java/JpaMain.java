786613c9787909995db9391ca0e6f6d112fa2dae
“funhappyit” <fun.happyit@gmail.com>
2024-03-24 20:27:59 +0900

    2023-03-24 단방향 연관관계

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,7 +1,11 @@
+import model.Member;
+import model.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         try{
             tx.begin(); //[트랜잭션] -시작
-            logic(em); //비즈니스 로직 실행
+            deleteRelation(em); //비즈니스 로직 실행
             tx.commit(); //[트랜잭션] - 커밋
         }catch (Exception e){
             tx.rollback(); //[트랜잭션] - 롤백
@@ -27,7 +31,52 @@ public class JpaMain {
 
     //비즈닌스 로직
     private static void logic(EntityManager em){
-        Order order = em.find(Order.class,1);
-        
+        //팀 1 저장
+        Team team1 = new Team("team1","팀1");
+        em.persist(team1);
+
+        //회원1 저장
+        Member member1 = new Member("member1","회원1");
+        member1.setTeam(team1); //연관관계 설정 member1-> team1
+        em.persist(member1);
+
+        //회원2 저장
+        Member member2 = new Member("member2","회원2");
+        member2.setTeam(team1); //연관관계 설정 member2->team1
+        em.persist(member2);
     }
+
+    //JPQL 조인 검색
+    private static void queryLogicJoin(EntityManager em){
+        String jpq1 = "select m from Member m join m.team t where "+"t.name=:teamName";
+
+        List<Member> resultList = em.createQuery(jpq1,Member.class)
+                .setParameter("teamName","팀1")
+                .getResultList();
+
+        for(Member member:resultList){
+            System.out.println("[query] member.username="+member.getUsername());
+        }
+    }
+    //수정
+    private static void updateRelation(EntityManager em){
+        //새로운 팀2
+        Team team2 = new Team("team2","팀2");
+        em.persist(team2);
+
+        //회원1에 새로운 팀2 설정
+        Member member = em.find(Member.class,"member1");
+        member.setTeam(team2);
+    }
+    //연관관계를 삭제하는 코드
+    private static void deleteRelation(EntityManager em){
+        Member member1 = em.find(Member.class,"member1");
+        Member member2 = em.find(Member.class,"member2");
+        member1.setTeam(null);
+        member2.setTeam(null);
+        Team team = em.find(Team.class,"team1");
+        em.remove(team);
+
+    }
+
 }
